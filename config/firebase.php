595ec778a717cf271c44db885eb5d8543a18a9c9
FantaTine <fantandao.tine@unchk.edu.sn>
2024-09-28 18:44:36 +0000

    Nouvelle image docker

--- config/firebase.php
+++ config/firebase.php
@@ -29,9 +29,10 @@ return [
              */
 
             'credentials' => [
-                'project_id' => env('FIREBASE_PROJECT_ID'),
-                'client_email' => env('FIREBASE_CLIENT_EMAIL'),
-                'private_key' => str_replace('\\n', "\n", env('FIREBASE_PRIVATE_KEY')),
+                #'project_id' => env('FIREBASE_PROJECT_ID'),
+                #'client_email' => env('FIREBASE_CLIENT_EMAIL'),
+                #'private_key' => str_replace('\\n', "\n", env('FIREBASE_PRIVATE_KEY')),
+                'file' => env('FIREBASE_CREDENTIALS', base_path('firebase-key.json')),
                 
             ],
 
