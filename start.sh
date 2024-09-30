0c0f6bfef7de16df0eaa7978272d9180348822fd
FantaTine <fantandao.tine@unchk.edu.sn>
2024-09-30 12:40:39 +0000

    Redeploiement

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-php artisan config:cache
-php artisan route:cache
-php artisan view:cache
+   php artisan config:cache
+   php artisan route:cache
+   php artisan view:cache
 
-php-fpm
+   php artisan serve --host=0.0.0.0 --port=$PORT
